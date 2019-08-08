b9abff1d822bce85fcf36c90e81c4bb5db064204
DmitryDzz <DzakhovDS@prosense.tv>
2019-08-08 12:02:00 +0300

    First commit

---
+++ Assets/BugDemo.cs
@@ -0,0 +1,30 @@
+﻿using UMP;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+public class BugDemo : MonoBehaviour
+{
+    
+/*
+ * How to reproduce the bug:
+ * 1. Run the app on an Android device.
+ * 2. Press Play button.
+ * 3. After the video has been started, press Quit.
+ * 4. Run the app again.
+ * 5. Press Play button.
+ * 6. Crash.
+ */
+    
+    public void PlayButtonHandler()
+    {
+        UniversalMediaPlayer player = FindObjectOfType<UniversalMediaPlayer>();
+        Assert.IsNotNull(player);
+        player.Path = "rtmp://vd2.wmspanel.com/video_demo/stream";
+        player.Play();
+    }
+
+    public void QuitButtonHandler()
+    {
+        Application.Quit();
+    }
+}
